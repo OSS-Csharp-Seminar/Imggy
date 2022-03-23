c11cc5dccf6d196c9d338c6d86cc9d74e8a7ff8b
HrvojeCukusicOSS <hrvoje.cukusic2342@gmail.com>
2022-03-23 10:25:03 +0100

    Added entitiies and updated persistence

---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public User User { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+        public DateTime DateOfCreation { get; set; }
+        public List<Album_Post> Album_Post { get; set; }
+        public List<PostLikes> PostLikes { get; set; }
+        public List<Post_Tag> Post_Tag { get; set; }
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User : IdentityUser
+    {
+        public DateTime DateOfCreation { get; set; }
+        public List<PostLikes> PostLikes { get; set; }
+        public List<CommentLikes> CommentLikes { get; set; }
+    }
+}
