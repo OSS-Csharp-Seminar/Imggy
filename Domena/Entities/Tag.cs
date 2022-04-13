ac2c0344f9bbe944a899b49ed96877e38de6563a
HrvojeCukusicOSS <77282204+HrvojeCukusicOSS@users.noreply.github.com>
2022-04-13 19:57:04 +0200

    Cukusic (#4)
    * Updated skelleton
    
    * Updated project structure and added Database context, but couldn't migration
    
    Co-authored-by: HrvojeCukusicOSS <hrvoje.cukusic2342@gmail.com>

---
+++ Domena/Entities/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Tag
+    {
+        public Tag()
+        {
+            this.posts = new HashSet<Post>();
+        }
+        public int id { get; set; }
+        public string name { get; set; }
+        public virtual ICollection<Post> posts { get; set; }
+    }
+}
---
+++ Persistence/DatabaseContext/ImggyDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.DbInfrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.DatabaseContext
+{
+    class ImggyDbContextFactory : DesignTimeDbContextFactoryBase<ImggyDbContext>
+    {
+        protected override ImggyDbContext CreateNewInstance(DbContextOptions<ImggyDbContext> options)
+        {
+            return new ImggyDbContext(options);
+        }
+    }
+}
