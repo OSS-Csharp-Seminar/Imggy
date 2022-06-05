e437bceaa4077b7321fb42159df8157394011e49
KarloMagdic <karlom1999@gmail.com>
2022-06-05 09:49:37 +0200

    Added dtos

---
+++ Domain/Dtos/AlbumDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Dtos
+{
+    internal class AlbumDto
+    {
+        public string Name { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
--- Domain/Dtos/UserDto.cs
+++ Domain/Dtos/UserDto.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Service.DtoModels
+namespace Domain.Dtos
 {
-    public class UserDto
+    internal class UserDto
     {
         public string UserName { get; set; }
-        public string Email { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
         public string Role { get; set; }
     }
 }
