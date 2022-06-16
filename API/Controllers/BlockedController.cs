bfe2dc4dbb95215df916f6544792cd21d35d0365
KarloMagdic <karlom1999@gmail.com>
2022-06-16 22:14:49 +0200

    Added Controllers

---
+++ API/Controllers/BlockedController.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Persistance.Repositories;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlockedController : BaseController<Blocked>
+    {
+        public BlockedController(BlockedRepository Repository) : base(Repository)
+        {
+        }
+    }
+}
